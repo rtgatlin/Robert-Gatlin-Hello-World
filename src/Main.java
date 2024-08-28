927e589a8905c1e7e8709ae0cfc02452a7abb659
rtgatlin <rgatlin@cub.uca.edu>
2024-08-28 14:15:31 -0500

    Hello World

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class Hello
+{
+    public static void main(String[]args)
+    {
+        System.out.println("Hello World!");
+    }
+}
